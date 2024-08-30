9a2af2265f0843eaa170beca2b8118b7e6c735cf
Achraf2500 <charafe.boulhout@univ-constantine2.dz>
2024-08-30 18:05:55 +0200

    open graph fix

--- components/LinkPreview.vue
+++ components/LinkPreview.vue
@@ -1,6 +1,34 @@
 <template>
-    <div>
-        {{ ogData }}
+    <div v-if="ogData" class="flex space-x-4 border rounded-md overflow-hidden bg-white dark:border-gray-900 dark:bg-slate-800 mt-4">
+        <div class="w-40 h-40 ">
+            <img :src="ogData['og:image']" class="w-full h-full object-cover" alt="" srcset="">
+        </div>
+        <div class="flex flex-col p-2">
+            <div class="flex flex-1 flex-col">
+                <h2 class="font-bold text-lg line-clamp-2 mb-2">{{ ogData['og:title'] }}</h2>
+                <p class="line-clamp-4 text-xs text-gray-400">
+                    {{ ogData['og:description'] }}
+                </p>
+            </div>
+            <nuxt-link target="_blank" class="text-sm text-gray-400" :to="ogData['og:url']">{{ ogData['og:url'] }}</nuxt-link>
+        </div>
+    </div>
+    <div class="flex space-x-4 border rounded-md overflow-hidden bg-white dark:border-gray-900 dark:bg-[#0f172a] " v-else>
+        <div class="w-40 h-40 ">
+            <USkeleton class="w-full h-full" />
+        </div>
+        <div class="flex flex-col p-2 py-4">
+            <div class="flex flex-1 flex-col space-y-2">
+                <USkeleton class="w-48 h-4 mb-2" />
+                
+                <div class="flex flex-col space-y-2">
+                    <USkeleton class="w-full h-2" />
+                    <USkeleton class="w-full h-2" />
+                    <USkeleton class="w-1/3 h-2" />
+                </div>
+            </div>
+            <USkeleton class="w-full h-2" />
+        </div>
     </div>
 </template>
 
@@ -12,8 +40,10 @@ const error = ref('');
 
 
 const props = defineProps({
-    url: { type: String, required: false },
+    url: { type: String },
 })
+
+console.log(props.url);
 
 
 const fetchOgData = async () => {
@@ -40,7 +70,7 @@ const fetchOgData = async () => {
     }
 }
 
-onMounted( async () => {
+onMounted(async () => {
     await fetchOgData()
 })
 </script>
